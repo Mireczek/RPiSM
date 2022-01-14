52d9d1d8f3c7c3302be38ee1e5e7f3dd4fbc3d59
Mirotiroriro <mirek2220@gmail.com>
2022-01-14 16:58:48 +0100

    Update main.py

--- main.py
+++ main.py
@@ -4,52 +4,38 @@ import numpy
 kostka1 = [1,2,6,7]
 kostka2 = [3,4,4,8]
 k = 4
-liczbaRzutow = 50
+liczbaWylosowanych = 50
 
-def rzucaj(kostka):
+def udawajZeLosujeszZRozkladu(kostka):
     rzuty = []
-    for i in range(liczbaRzutow):
-        rzuty.append(random.choice(kostka))
+    for i in range(liczbaWylosowanych):
+        wynik = 0
+        for z in range(k):
+            wynik += random.choice(kostka)
+        rzuty.append(wynik)
     return rzuty
 
-def sumujRzuty(kostka):
-    wynik = 0
-    for i in range(k):
-        wynik += random.choice(kostka)
-    return wynik
+def testRownosciSrednich(srednia1, srednia2, wariancja1, wariancja2):
+    mianownik = numpy.sqrt((wariancja1/liczbaWylosowanych)+(wariancja2/liczbaWylosowanych))
+    return (srednia1 - srednia2)/mianownik
 
-def rzucajDlaSumy(kostka):
-    rzuty = []
-    for i in range(liczbaRzutow):
-        rzuty.append(sumujRzuty(kostka))
-    return rzuty
+def testRownosciWariancji(wariancja1, wariancja2):
+    return numpy.maximum(wariancja1,wariancja2)/numpy.minimum(wariancja1,wariancja2)
 
-rzutyKostka1 = []
-rzutyKostka2 = []
-sumaRzutow1 = []
-sumaRzutow2 = []
+wylosowaneZRozkladuKostka1 = udawajZeLosujeszZRozkladu(kostka1)
+wylosowaneZRozkladuKostka2 = udawajZeLosujeszZRozkladu(kostka2)
 
-rzutyKostka1 = rzucaj(kostka1)
-rzutyKostka2 = rzucaj(kostka2)
-sumaRzutow1 = rzucajDlaSumy(kostka1)
-sumaRzutow2 = rzucajDlaSumy(kostka2)
+sredniaR1 = numpy.mean(wylosowaneZRozkladuKostka1)
+sredniaR2 = numpy.mean(wylosowaneZRozkladuKostka2)
 
-sredniaR1 = numpy.mean(rzutyKostka1)
-sredniaR2 = numpy.mean(rzutyKostka2)
-sredniaSR1 = numpy.mean(sumaRzutow1)
-sredniaSR2 = numpy.mean(sumaRzutow2)
+wariancjaR1 = numpy.var(wylosowaneZRozkladuKostka1)
+wariancjaR2 = numpy.var(wylosowaneZRozkladuKostka2)
 
-wariancjaR1 = numpy.var(rzutyKostka1)
-wariancjaR2 = numpy.var(rzutyKostka2)
-wariancjaSR1 = numpy.var(sumaRzutow1)
-wariancjaSR2 = numpy.var(sumaRzutow2)
+print("Średnia dla wylosowanych z rozkładu kostki 1 : " + str(sredniaR1))
+print("Średnia dla wylosowanych z rozkładu kostki 2 : " + str(sredniaR2))
 
-print("Średnia dla kostki 1 : " + str(sredniaR1))
-print("Średnia dla kostki 2 : " + str(sredniaR2))
-print("Średnia dla sumy kostki 1 : " + str(sredniaSR1))
-print("Średnia dla sumy kostki 2 : " + str(sredniaSR2))
+print("Wariancja dla wylosowanych z rozkładu kostki 1 : " + str(wariancjaR1))
+print("Wariancja dla wylosowanych z rozkładu kostki 2 : " + str(wariancjaR2))
 
-print("Wariancja dla kostki 1 : " + str(wariancjaR1))
-print("Wariancja dla kostki 2 : " + str(wariancjaR2))
-print("Wariancja dla sumy kostki 1 : " + str(wariancjaSR1))
-print("Wariancja dla sumy kostki 2 : " + str(wariancjaSR2))
+print("Test równości średnich : " + str(testRownosciSrednich(sredniaR1,sredniaR2,wariancjaR1,wariancjaR2)))
+print("Test równości wariancji : " + str(testRownosciWariancji(wariancjaR1,wariancjaR2)))
